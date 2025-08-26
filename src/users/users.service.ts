00331a5caddca4be9343b44a592851680012879c
zimam ahmed <zimam.ahmed@oioi.biz>
2025-08-26 15:33:41 +0500

    repo hooked with user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>){
+        this.repo = repo;
+    }        
+}
