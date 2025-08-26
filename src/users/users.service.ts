22fa6257e17bd07909e8747adfdf61b22df333f7
zimam ahmed <zimam.ahmed@oioi.biz>
2025-08-26 16:10:26 +0500

    Create user added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,11 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>){
-        this.repo = repo;
-    }        
+    constructor(@InjectRepository(User) private repo: Repository<User>){}
+    
+    create(email: string, password: string){
+        const user = this.repo.create({email, password});
+
+        return this.repo.save(user);
+    }
 }
