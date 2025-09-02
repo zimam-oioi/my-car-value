85fa28e18a76a6d4bf178d889681e309a38e40db
zimam ahmed <zimam.ahmed@oioi.biz>
2025-09-02 16:39:33 +0500

    user apis are set

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Patch, Param, Query, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -14,9 +14,12 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string){
-
-        return this.userService.findOne(parseInt(id));
+    async findUser(@Param('id') id: string){
+        const user = await this.userService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('not found');
+        }
+        return user;
     }
 
     @Get()
@@ -29,7 +32,7 @@ export class UsersController {
         console.log(id)
         return this.userService.remove(parseInt(id));
     }
-
+// updating user
     @Patch(':id')
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
         return this.userService.update(parseInt(id), body);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -6,7 +6,7 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>){}
-    
+    // using async await for db calls
     async create(email: string, password: string){
        
         const user = this.repo.create({email, password}); 
@@ -27,7 +27,7 @@ export class UsersService {
     async update(id: number, attrs: Partial<User>){
         const user = await this.findOne(id);
         if(!user){
-            throw new Error('User Not Found');
+            throw new NotFoundException('User Not Found');
         }
         Object.assign(user, attrs);
         return this.repo.save(user);
@@ -37,7 +37,7 @@ export class UsersService {
      const user = await this.findOne(id);
      console.log(user)
       if(!user){
-            throw new Error('User Not Found');
+            throw new NotFoundException('User Not Found');
       }
       return this.repo.remove(user)  
 
